f5b8095447bb3f9fd6943c37c4e98f25e40f6082
user <user@DESKTOP-EOQAJOP>
2022-12-28 08:25:22 -0500

    Segunda subida de proyecto para herramientas IV - 2022-II

--- HIV2022/src/hiv2022/HIV2022.java
+++ HIV2022/src/hiv2022/HIV2022.java
@@ -6,6 +6,11 @@ public class HIV2022 {
     
     public static void main(String[] args) {
         
+        int x = 4;
+        int y = 5;
+        int suma = x + y;
+        System.out.println("La suma es:"+suma);
+        
     }
     
 }
