70aac6c9d75195c31b0b07e1aed626ef44b74828
OscarSDK <OscarSDK@gmail.com>
2022-12-29 07:33:39 -0500

    Tercera actualización para H4 2022-II

--- HIV2022/src/hiv2022/HIV2022.java
+++ HIV2022/src/hiv2022/HIV2022.java
@@ -8,7 +8,8 @@ public class HIV2022 {
         
         int x = 4;
         int y = 5;
-        int suma = x + y;
+        int z = 7;
+        int suma = x + y + z;
         System.out.println("La suma es:"+suma);
         
     }
